105e640af55f2c4ef102c836fe76e3b22db30a38
Kim Changju <zxagsw@gmail.com>
2019-05-22 17:15:40 +0900

    소스트리 버그때문에 다시커

--- map_test/assets/js/map.js
+++ map_test/assets/js/map.js
@@ -22,7 +22,7 @@ function panTo() {
     var moveLatLon = new daum.maps.LatLng(33.450580, 126.574942);
     
     // 지도 중심을 부드럽게 이동시킵니다
-    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다dddd
     map.panTo(moveLatLon);            
 }
 }
