22fb3dd87fc49f42a5fc85035859b863585cdee1
Kim Changju <zxagsw@gmail.com>
2019-05-22 16:38:03 +0900

    map move add

--- html_test/assets/js/main.js
+++ html_test/assets/js/main.js
@@ -3,6 +3,14 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+function panTo() {
+    // 이동할 위도 경도 위치를 생성합니다 
+    var moveLatLon = new daum.maps.LatLng(33.450580, 126.574942);
+    
+    // 지도 중심을 부드럽게 이동시킵니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    map.panTo(moveLatLon);            
+}
 
 (function($) {
 
