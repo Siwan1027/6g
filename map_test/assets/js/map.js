042628e62d8b5d38d81e9f260ccff1339643543c
Kim Changju <zxagsw@gmail.com>
2019-05-22 17:33:43 +0900

    map panto 구

--- map_test/assets/js/map.js
+++ map_test/assets/js/map.js
@@ -1,9 +1,8 @@
-
-	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+mapOption = { 
+center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+level: 3 // 지도의 확대 레벨
+};
 
 var map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
@@ -15,9 +14,9 @@ function setCenter() {
     map.setCenter(moveLatLon);
 }
 
-function panTo() {
+function panTo(x, y) {
     // 이동할 위도 경도 위치를 생성합니다 
-    var moveLatLon = new daum.maps.LatLng(33.450580, 126.574942);
+    var moveLatLon = new daum.maps.LatLng(x, y);
     
     // 지도 중심을 부드럽게 이동시킵니다
     // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다dddd
