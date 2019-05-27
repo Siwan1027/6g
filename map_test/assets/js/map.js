32375179db18f1ac3b55e9bfd60813a5bf4d9170
김창주 <zxagsw@naver.com>
2019-05-27 10:22:52 +0900

    map main edit

--- map_test/assets/js/map.js
+++ map_test/assets/js/map.js
@@ -1,6 +1,6 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 mapOption = { 
-center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+center: new daum.maps.LatLng(36.815105, 127.113886), // 지도의 중심좌표
 level: 3 // 지도의 확대 레벨
 };
 
