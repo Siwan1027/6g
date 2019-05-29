8e2cd124a86829bb59d83ad0d978f70d2ae9ba91
Kim Changju <zxagsw@gmail.com>
2019-05-29 16:39:05 +0900

    되돌리

--- map_test/assets/js/map.js
+++ map_test/assets/js/map.js
@@ -36,7 +36,7 @@ function searchPlaces() {
 function placesSearchCB(data, status, pagination) {
     if (status === daum.maps.services.Status.OK) {
 		setHtml("school", data.length);
-		setHtml("text", data.place.name);
+		setHtml("text", data.name);
 	    } else if (status === daum.maps.services.Status.ZERO_RESULT) {
         alert("검색결과없음");
     } else if (status === daum.maps.services.Status.ERROR) {
