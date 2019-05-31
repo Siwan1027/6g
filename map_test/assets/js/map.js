d14757a7894934209691d24a04aa4a86735516b8
Kim Changju <zxagsw@gmail.com>
2019-05-31 18:23:30 +0900

    자세한 목록의 이름 누르면 지도에서 표시, 주소 누르면 다음 지도 설명 페이지로 이동 추가

--- map_test/assets/js/map.js
+++ map_test/assets/js/map.js
@@ -13,6 +13,7 @@ var ps = new daum.maps.services.Places(map);
 var category = new Array("MT1", "CS2", "PS3", "SC4", "SW8", "PO3", "FD6"); //카테고리 코드
 var categoryCount = 0; // 카테고리 카운트
 var mart, conv, kids, school, station, public, rest;
+var markers = [];
 
 // 지도 타입 컨트롤을 지도에 표시합니다
 map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
@@ -25,6 +26,7 @@ var marker = new daum.maps.Marker({
 });
 // 지도에 마커를 표시합니다
 marker.setMap(map);
+markers.push(marker);
 
 // 지도에 클릭 이벤트를 등록합니다
 // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
@@ -40,6 +42,13 @@ daum.maps.event.addListener(map, 'click', function(mouseEvent) {
 });
 
 function action(x, y) {
+	removeMarker();
+	var detail = document.getElementById("detailView");
+	if (detail == null) {
+		alert("detailView" + ' 찾기 오류'); 
+		return;
+	 }
+	detail.innerHTML = "";
 	map.setLevel(3);
 	var moveLatLon = new daum.maps.LatLng(x, y);
 	var markerPosition  = new daum.maps.LatLng(x, y);
@@ -48,6 +57,7 @@ function action(x, y) {
     position: markerPosition
 	});
 	marker.setMap(map);
+	markers.push(marker);
 	placeSearch();
 }
 
@@ -113,7 +123,7 @@ function placesSearchCB(data, status, pagination) {
     }
 }
 
-function setHtml(item_id, item_html, place)
+function setHtml(item_id, item_html)
 {
 	 obj = document.getElementById(item_id);
 	 if (obj == null) {
@@ -123,60 +133,81 @@ function setHtml(item_id, item_html, place)
 	 obj.innerHTML = item_html;
 } // html 내용바꾸기
 
+function removeMarker() {
+    for ( var i = 0; i < markers.length; i++ ) {
+        markers[i].setMap(null);
+    }   
+    markers = [];
+}
+
 function detailView(cname){
-	var detail = document.getElementById("text");
-	detail.innerHTML = "";
-	 if (detail == null) {
-		alert("text" + ' 찾기 오류'); 
+	var detail = document.getElementById("detailView");
+	if (detail == null) {
+		alert("detailView" + ' 찾기 오류'); 
 		return;
 	 }
+	detail.innerHTML = "";
+
 	if(cname === "mart"){
 		for(var i=0; i<mart.length; i++){
-			newP = document.createElement('p');
-			detail.appendChild(newP);
-			newP.innerHTML = mart[i].place_name;
+			newTR = document.createElement('tr');
+			detail.appendChild(newTR);
+			newTR.innerHTML = "<th scope=\'row\' onClick=geoMarkerAdd(" + mart[i].y + "," + mart[i].x + "); style=\"cursor:pointer;\">" + mart[i].place_name + "</th>" + "<td onClick=\"window.open(\'" + mart[i].place_url + "\',\'\',\'\');\" style=\"cursor:pointer;\">" + mart[i].road_address_name + "</td> <td>" + mart[i].phone + "</td>";
 		}
 	}
 	else if(cname === "conv"){
 		for(var i=0; i<conv.length; i++){
-			newP = document.createElement('p');
-			detail.appendChild(newP);
-			newP.innerHTML = conv[i].place_name;
+			newTR = document.createElement('tr');
+			detail.appendChild(newTR);
+			newTR.innerHTML = "<th scope=\'row\' onClick=geoMarkerAdd(" + conv[i].y + "," + conv[i].x + "); style=\"cursor:pointer;\">" + conv[i].place_name + "</th>" + "<td onClick=\"window.open(\'" + conv[i].place_url + "\',\'\',\'\');\" style=\"cursor:pointer;\">" + conv[i].road_address_name + "</td> <td>" + conv[i].phone + "</td>";
 		}
 	}
 	else if(cname === "kids"){
 		for(var i=0; i<kids.length; i++){
-			newP = document.createElement('p');
-			detail.appendChild(newP);
-			newP.innerHTML = kids[i].place_name;
+			newTR = document.createElement('tr');
+			detail.appendChild(newTR);
+			newTR.innerHTML = "<th scope=\'row\' onClick=geoMarkerAdd(" + kids[i].y + "," + kids[i].x + "); style=\"cursor:pointer;\">" + kids[i].place_name + "</th>" + "<td onClick=\"window.open(\'" + kids[i].place_url + "\',\'\',\'\');\" style=\"cursor:pointer;\">" + kids[i].road_address_name + "</td> <td>" + kids[i].phone + "</td>";
 		}
 	}
 	else if(cname === "school"){
 		for(var i=0; i<school.length; i++){
-			newP = document.createElement('p');
-			detail.appendChild(newP);
-			newP.innerHTML = school[i].place_name;
+			newTR = document.createElement('tr');
+			detail.appendChild(newTR);
+			newTR.innerHTML = "<th scope=\'row\' onClick=geoMarkerAdd(" + school[i].y + "," + school[i].x + "); style=\"cursor:pointer;\">" + school[i].place_name + "</th>" + "<td onClick=\"window.open(\'" + school[i].place_url + "\',\'\',\'\');\" style=\"cursor:pointer;\">" + school[i].road_address_name + "</td> <td>" + school[i].phone + "</td>";
 		}
 	}
 	else if(cname === "station"){
 		for(var i=0; i<station.length; i++){
-			newP = document.createElement('p');
-			detail.appendChild(newP);
-			newP.innerHTML = station[i].place_name;
+			newTR = document.createElement('tr');
+			detail.appendChild(newTR);
+			newTR.innerHTML = "<th scope=\'row\' onClick=geoMarkerAdd(" + station[i].y + "," + station[i].x + "); style=\"cursor:pointer;\">" + station[i].place_name + "</th>" + "<td onClick=\"window.open(\'" + station[i].place_url + "\',\'\',\'\');\" style=\"cursor:pointer;\">" + station[i].road_address_name + "</td> <td>" + station[i].phone + "</td>";
 		}
 	}
 	else if(cname === "public"){
 		for(var i=0; i<public.length; i++){
-			newP = document.createElement('p');
-			detail.appendChild(newP);
-			newP.innerHTML = public[i].place_name;
+			newTR = document.createElement('tr');
+			detail.appendChild(newTR);
+			newTR.innerHTML = "<th scope=\'row\' onClick=geoMarkerAdd(" + public[i].y + "," + public[i].x + "); style=\"cursor:pointer;\">" + public[i].place_name + "</th>" + "<td onClick=\"window.open(\'" + public[i].place_url + "\',\'\',\'\');\" style=\"cursor:pointer;\">" + public[i].road_address_name + "</td> <td>" + public[i].phone + "</td>";
 		}
 	}
 	else if(cname === "rest"){
 		for(var i=0; i<rest.length; i++){
-			newP = document.createElement('p');
-			detail.appendChild(newP);
-			newP.innerHTML = rest[i].place_name;
+			newTR = document.createElement('tr');
+			detail.appendChild(newTR);
+			newTR.innerHTML = "<th scope=\'row\' onClick=geoMarkerAdd(" + rest[i].y + "," + rest[i].x + "); style=\"cursor:pointer;\">" + rest[i].place_name + "</th>" + "<td onClick=\"window.open(\'" + rest[i].place_url + "\',\'\',\'\');\" style=\"cursor:pointer;\">" + rest[i].road_address_name + "</td> <td>" + rest[i].phone + "</td>";
 		}
 	}
 } // 상세정보 보기 구현
+
+function geoMarkerAdd(x, y){
+	removeMarker();
+	map.setLevel(1);
+	var moveLatLon = new daum.maps.LatLng(x, y);
+	var markerPosition  = new daum.maps.LatLng(x, y);
+    map.panTo(moveLatLon);
+	var marker = new daum.maps.Marker({
+    position: markerPosition
+	});
+	marker.setMap(map);
+	markers.push(marker);
+}
